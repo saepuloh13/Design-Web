1eb89b6e4fd3aa44338c3a6fee66817c226fd8fe
Saepuloh <saepuloh_februari@gmail.com>
2018-10-11 19:41:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import Profile from './containers/Profile';
+import Home from './containers/Home';
+import Portofolio from './containers/Portofolio';
+import Contact from './containers/Contact';
+import FootText from './components/FootText';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+
+        <Header />
+        <Switch>
+          <Route path="/home" exact render={props => <Home />} />
+          <Route path="/profil" render={props => <Profile />} />
+          <Route path="/portofolio" render={props => <Portofolio />} />
+          <Route path="/contact" render={props => <Contact />} />
+        </Switch>
+        <FootText />
+
+      </React.Fragment>
     );
   }
 }
